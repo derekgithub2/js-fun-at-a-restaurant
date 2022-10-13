45df07da3dc29aca5e0fbe49500fe0b62cbe93ba
Derek Yeh <derek.yeh91@gmail.com>
2022-10-12 20:45:56 -0700

    completel warm up

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,33 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return food = `Delicious ${food}`
+};
 
-}
+function createMenuItem(name, price, type) {
+  return menuItemName = { name, price, type }
+};
+
+// var ingredients = [];
+
+function addIngredients(newFood, ingredients) {
+  return ingredients.push(newFood);
+  // console.log(addIngredients.ingredients);
+};
+// addIngredients("cheese", ingredients);
+// addIngredients("cheese", ingredients);
+
+// var checkRepeat = ingredients.indexOf(newFood);
+// while (checkRepeat !== -1) {
+//   indices.push(checkRepeat);
+//   checkRepeat = ingredients.indexOf(newFood, checkRepeat + 1);
+// }
+// console.log(checkRepeat);
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
