9e2bc60d4ac95d94104a3f4a4752ddd6f1374f07
Derek Yeh <derek.yeh91@gmail.com>
2022-10-14 10:37:28 -0700

    finish menu-test

--- src/meal.js
+++ src/meal.js
@@ -3,28 +3,42 @@ function nameMenuItem(food) {
 };
 
 function createMenuItem(name, price, type) {
-  return menuItemName = { name, price, type }
+  return menuItemName = {
+    name,
+    price,
+    type
+  }
 };
-
-// var ingredients = [];
 
 function addIngredients(newFood, ingredients) {
-  this.ingredients = [];
-  if (this.ingredients.indexOf(newFood) === -1) {
-    return this.ingredients.push(newFood);
-    // console.log(this.ingredients);
-  }
-  if (newFood = this.ingredients.indexOf(newFood)) {
-    return this.ingredients.pop(newFood);
+  if (ingredients.indexOf(newFood) === -1) {
+    return ingredients.push(newFood);
+  } else {
+    return ingredients;
   }
 };
 
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice*0.9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
